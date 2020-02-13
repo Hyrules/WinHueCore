33c5b2e14cbf5aa8bbb9451c47407ab7740e1602
Gogs <gogs@fake.local>
2020-02-13 13:28:18 -0500

    WIP Mainform

---
+++ WinHue Core/MainForm/MainFormViewModel_Properties.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WinHue_Core.MVVM;
+using WinHue_Core.Philips_Hue;
+
+namespace WinHue_Core.MainForm
+{
+    public partial class MainFormViewModel : ViewModelBase
+    {
+        private List<Bridge> _listBridges;
+
+        public MainFormViewModel()
+        {
+            _listBridges = new List<Bridge>();
+        }
+    }
+}
